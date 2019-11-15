a2095ec733d626be17de39e1f75cf8ab1f079a73
Brandon Frazier <brandonfrazier@Brucebs-MacBook-Pro.local>
2019-11-15 16:34:30 -0700

    Filled out some information before running into JNI error

--- src/MakeChangeApp.java
+++ src/MakeChangeApp.java
@@ -1,9 +1,29 @@
 
 public class MakeChangeApp {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static java.util.Scanner scanner = new java.util.Scanner(System.in);
 
+	public static void main(String[] args) {
+		
+
+		double quarters = .25;
+		double dimes = .10;
+		double nickels = .05;
+		double pennies = .01;
+		double twenty = 20.0;
+		double ten = 10.0;
+		double five = 5.0;
+		double one = 1.0;
+		double cost = 0.0;
+		double payment = 0.0;
+		
+		System.out.println("Enter the amount of money charged for the product: ");
+		cost = scanner.nextDouble();
+
+		System.out.println("Enter the amount of money received for the product: ");
+		payment = scanner.nextDouble();
+		
+		
 	}
 
 }
