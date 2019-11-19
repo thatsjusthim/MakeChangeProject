bbfd89fdaa152b28919efa7ed8cc1312d3cf8c15
Brandon Frazier <frazierbrandon@outlook.com>
2019-11-19 14:23:40 -0700

    Trying again

--- src/MakeChangeApp.java
+++ src/MakeChangeApp.java
@@ -6,16 +6,10 @@ public class MakeChangeApp {
 	public static void main(String[] args) {
 		
 
-		double quarters = .25;
-		double dimes = .10;
-		double nickels = .05;
-		double pennies = .01;
-		double twenty = 20.0;
-		double ten = 10.0;
-		double five = 5.0;
-		double one = 1.0;
-		double cost = 0.0;
-		double payment = 0.0;
+		double quarters = .25, dimes = .10, nickels = .05, pennies = .01, twenty = 20.0, ten = 10.0, 
+				five = 5.0, one = 1.0, cost = 0.0, payment = 0.0; 
+		
+		
 		
 		System.out.println("Enter the amount of money charged for the product: ");
 		cost = scanner.nextDouble();
@@ -23,10 +17,38 @@ public class MakeChangeApp {
 		System.out.println("Enter the amount of money received for the product: ");
 		payment = scanner.nextDouble();
 		
+		if(payment < cost) {
+		System.out.println("This payment doesn't cover the purchase price. ");
+		}
+		else if(payment == cost) {
+		System.out.println("Thank you for providing exact payment! There is no change due. ");
+		}
+		
+		if(payment > cost) {
+			System.out.println("You are owed $" + (payment - cost) + " in change! ");
+		}
+		double changeOwed = payment - cost;
+		
+		System.out.println(changeOwed);
+		
+		double changeRemaining = (changeOwed % 20.0);
 		
 		
-	}
+		changeRemaining = (changeOwed % 10.0);
 		
-	
-
+		changeRemaining = (changeRemaining % 5.0);
+		
+		changeRemaining = (changeRemaining % 1.0);
+		
+		changeRemaining = (changeRemaining % .25);
+		
+		changeRemaining = (changeRemaining % .10);
+		
+		changeRemaining = (changeRemaining % .05);
+		
+		changeRemaining = (changeRemaining % .01);
+		
+		System.out.println(changeRemaining);
+	} 
+		
 }
